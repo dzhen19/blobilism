c39029c702b45ec2d22168f894f4695036dc3aff
Derrick Zhen <derrickzhen@wc123-dhcp-154.brynmawr.edu>
2023-01-26 16:24:19 -0500

    done

--- src/blobilism.cpp
+++ src/blobilism.cpp
@@ -2,19 +2,32 @@
 #include <vector>
 #include "tinygl-cpp.h"
 #include <string>
+#include <list>
 using namespace tinygl;
-// using namespace std;
+
+struct renderCircle
+{
+  float x;
+  float y;
+  int diameter;
+  std::string color;
+  float opacity;
+};
 
 class MyWindow : public Window
 {
 public:
   MyWindow(int w, int h) : Window(w, h) {}
 
-  float c1_x = 30, c1_y = 30, c2_x = 80, c2_y = 30, c3_x = 130,
-        c3_y = 30, c4_x = 180, c4_y = 30, c5_x = 230, c5_y = 30;
-  float diameter = 30;
-  float radius = diameter / 2;
-  std::string selectedColor = "c1";
+  float green_x = 30, green_y = 30, purple_x = 80, purple_y = 30, blue_x = 130,
+        blue_y = 30, yellow_x = 180, yellow_y = 30, orange_x = 230, orange_y = 30;
+
+  float paletteDiameter = 30;
+  float brushDiameter = 15;
+  float brushOpacity = 1;
+  float paletteRadius = paletteDiameter / 2;
+  std::list<renderCircle> strokes;
+  std::string selectedColor = "green";
 
   void setup() override
   {
@@ -26,14 +39,32 @@ public:
     return sqrt((p1_x - p2_x) * (p1_x - p2_x) + (p1_y - p2_y) * (p1_y - p2_y));
   }
 
+  virtual float clamp(float v, float lo, float hi)
+  {
+    if (v > hi)
+      return hi;
+    if (v < lo)
+      return lo;
+    return v;
+  }
+
   virtual void mouseMotion(int x, int y, int dx, int dy)
   {
     if (mouseIsDown(GLFW_MOUSE_BUTTON_LEFT))
     {
-      // todo: store a circle with the current color, size, x, y
-      // std::cout << "x: " << x << " y: " << y << " dx: " << dx << " dy: " << dy << std::endl;
-      color(0.051, 0.455, 0.102);
-      circle(x, y, 10);
+      if (y < 70)
+      {
+        return;
+      }
+
+      renderCircle newCircle;
+      newCircle.x = x;
+      newCircle.y = y;
+      newCircle.diameter = brushDiameter;
+      newCircle.opacity = brushOpacity;
+      newCircle.color = selectedColor;
+
+      strokes.push_back(newCircle);
     }
   }
 
@@ -46,34 +77,37 @@ public:
       float mx = mouseX(); // current mouse pos x
       float my = mouseY(); // current mouse pos y
 
-      float distanceC1 = getDistance(c1_x, c1_y, mx, my);
-      float distanceC2 = getDistance(c2_x, c2_y, mx, my);
-      float distanceC3 = getDistance(c3_x, c3_y, mx, my);
-      float distanceC4 = getDistance(c4_x, c4_y, mx, my);
-      float distanceC5 = getDistance(c5_x, c5_y, mx, my);
+      float distancegreen = getDistance(green_x, green_y, mx, my);
+      float distancepurple = getDistance(purple_x, purple_y, mx, my);
+      float distanceblue = getDistance(blue_x, blue_y, mx, my);
+      float distanceyellow = getDistance(yellow_x, yellow_y, mx, my);
+      float distanceorange = getDistance(orange_x, orange_y, mx, my);
 
-      if (distanceC1 < radius)
+      if (distancegreen < paletteRadius)
       {
-        selectedColor = "c1";
+        selectedColor = "green";
+        std::cout << selectedColor << " was selected." << std::endl;
       }
-      else if (distanceC2 < radius)
+      else if (distancepurple < paletteRadius)
       {
-        selectedColor = "c2";
+        selectedColor = "purple";
+        std::cout << selectedColor << " was selected." << std::endl;
       }
-      else if (distanceC3 < radius)
+      else if (distanceblue < paletteRadius)
       {
-        selectedColor = "c3";
+        selectedColor = "blue";
+        std::cout << selectedColor << " was selected." << std::endl;
       }
-      else if (distanceC4 < radius)
+      else if (distanceyellow < paletteRadius)
       {
-        selectedColor = "c4";
+        selectedColor = "yellow";
+        std::cout << selectedColor << " was selected." << std::endl;
       }
-      else if (distanceC5 < radius)
+      else if (distanceorange < paletteRadius)
       {
-        selectedColor = "c5";
+        selectedColor = "orange";
+        std::cout << selectedColor << " was selected." << std::endl;
       }
-      // std::cout << "distanceC1" << distanceC1 << "distanceC2" << distanceC2 << "distanceC3" << distanceC3 << "distanceC4" << distanceC4 << "distanceC5" << distanceC5 << std::endl;
-      std::cout << selectedColor << std::endl;
     }
   }
 
@@ -81,52 +115,94 @@ public:
   {
     if (key == GLFW_KEY_UP)
     {
-      // increase size of circle
+      brushDiameter = clamp(brushDiameter + 3, 5.0, 30.0);
+      std::cout << "brush diameter: " << brushDiameter << std::endl;
     }
     else if (key == GLFW_KEY_DOWN)
     {
-      // decrease size of circle
+      brushDiameter = clamp(brushDiameter - 3, 5.0, 30.0);
+      std::cout << "brush diameter: " << brushDiameter << std::endl;
     }
     else if (key == GLFW_KEY_LEFT)
     {
-      // decrease alpha
+      brushOpacity = clamp(brushOpacity - .1, 0.1, 1);
+      std::cout << "brush opacity: " << brushOpacity << std::endl;
     }
     else if (key == GLFW_KEY_RIGHT)
     {
-      // increase alpha
+      brushOpacity = clamp(brushOpacity + .1, 0.1, 1);
+      std::cout << "brush opacity: " << brushOpacity << std::endl;
     }
     else if (key == GLFW_KEY_C)
     {
-      // clear vector of circles
+      strokes.clear();
+      std::cout << "cleared screen!" << std::endl;
     }
   }
 
   void draw() override
   {
+
     background(0.95f, 0.95f, 0.95f); // parameters: r, g, b
 
-    color(1.0f, 0.5f, 0.5f);
-    circle(width() * 0.5f, height() * 0.5, 300);
+    // color(1.0f, 0.5f, 0.5f);
+    // circle(width() * 0.5f, height() * 0.5, 300);
 
     // todo : draw pallet
     color(0.1f, 0.1f, 0.1f);
     square(width() / 2.0f, 35, width(), 70);
 
-    // c1
+    // green
     color(0.051, 0.455, 0.102);
-    circle(c1_x, c1_y, diameter);
-    // c2
+    circle(green_x, green_y, paletteDiameter);
+    // purple
     color(0.227, 0.09, 0.447);
-    circle(c2_x, c2_y, diameter);
-    // c3
+    circle(purple_x, purple_y, paletteDiameter);
+    // blue
     color(0.325, 0.596, 0.745);
-    circle(c3_x, c3_y, diameter);
-    // c4
+    circle(blue_x, blue_y, paletteDiameter);
+    // yellow
     color(0.949, 0.804, 0.365);
-    circle(c4_x, c4_y, diameter);
-    // c5
+    circle(yellow_x, yellow_y, paletteDiameter);
+    // orange
     color(0.871, 0.647, 0.294);
-    circle(c5_x, c5_y, diameter);
+    circle(orange_x, orange_y, paletteDiameter);
+
+    // Create an iterator of std::list
+    std::list<renderCircle>::iterator it;
+    // Make iterate point to begining and incerement it one by one till it reaches the end of list.
+    for (it = strokes.begin(); it != strokes.end(); it++)
+    {
+      // Access the object through iterator
+      float x = it->x;
+      float y = it->y;
+      float diameter = it->diameter;
+      float opacity = it->opacity;
+      std::string _color = it->color;
+
+      if (_color == "green")
+      {
+        color(0.051, 0.455, 0.102, opacity);
+      }
+      else if (_color == "purple")
+      {
+        color(0.227, 0.09, 0.447, opacity);
+      }
+      else if (_color == "blue")
+      {
+        color(0.325, 0.596, 0.745, opacity);
+      }
+      else if (_color == "yellow")
+      {
+        color(0.949, 0.804, 0.365, opacity);
+      }
+      else if (_color == "orange")
+      {
+        color(0.871, 0.647, 0.294, opacity);
+      }
+
+      circle(x, y, diameter);
+    }
   }
 
 private:
